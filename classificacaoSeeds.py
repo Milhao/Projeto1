947ad344b0eae3f0980ab38029728c3be80b6c34
Milhao <leafar_rafaelsm@hotmail.com>
2016-09-23 17:20:47 -0300

    versao final seeds

--- classificacaoSeeds.py
+++ classificacaoSeeds.py
@@ -14,23 +14,24 @@ ds = SupervisedDataSet(7,1)
 trainSet = ClassificationDataSet(7, nb_classes=3, class_labels = ["one", "two", "three"])
 testSet = ClassificationDataSet(7, nb_classes=3)
 
-PorcDivTest = 0.25
-Ciclos = 700
-Momentum = 0.69;
-camada1 = 13
-camada2 = 0
+PorcDivTest = float(sys.argv[1])
+Ciclos = int(sys.argv[2])
+Learning = float(sys.argv[3])
+Momentum = float(sys.argv[4])
+camada1 = int(sys.argv[5])
+camada2 = int(sys.argv[6])
 
 k = 0
 size = 70
 for line in inputFile.readlines():
     data = [float(x) for x in line.strip().split() if x != '']
-    indata =  tuple(data[:7])
+    indata = tuple(data[:7])
     outdata = tuple(data[7:])
     ds.addSample(indata,outdata)
     k +=1
     if (k == size):
-		testdata, traindata = ds.splitWithProportion( PorcDivTest )    	
-		ds.clear() 	
+		testdata, traindata = ds.splitWithProportion( PorcDivTest )
+		ds.clear()
 		k = 0
 		for inp,targ in testdata:
 			testSet.appendLinked(inp,targ-1)
@@ -40,8 +41,8 @@ for line in inputFile.readlines():
 #print "**************************************"
 #print "Number of training patterns: ",len(testSet)
 #print "Input and output dimensions: ", testSet.indim, testSet.outdim
-#print testSet	
-#print "Number of training patterns: ",len(trainSet)	
+#print testSet
+#print "Number of training patterns: ",len(trainSet)
 #print "Input and output dimensions: ", trainSet.indim, trainSet.outdim
 #print trainSet
 #print trainSet['input'][90], trainSet['target'][90]
@@ -51,9 +52,6 @@ for line in inputFile.readlines():
 #print trainSet.getClass(0)
 #print(len(testSet.getField('target')))
 
-
-
-
 trainSet._convertToOneOfMany(bounds=[0, 1])
 testSet._convertToOneOfMany(bounds=[0, 1])
 #print(trainSet.getField('target'))
@@ -62,18 +60,16 @@ testSet._convertToOneOfMany(bounds=[0, 1])
 #print(testSet.getField('target'))
 
 if(camada2==0):
-	net = buildNetwork(trainSet.indim,camada1,trainSet.outdim, recurrent=True)
+	net = buildNetwork(trainSet.indim,camada1,trainSet.outdim, recurrent = True)
 else :
-	net = buildNetwork(trainSet.indim,camada1,camada2,trainSet.outdim, recurrent=True)
-trainer = BackpropTrainer(net,dataset = trainSet,learningrate=0.001,momentum=Momentum, verbose=True)
+	net = buildNetwork(trainSet.indim,camada1,camada2,trainSet.outdim, recurrent = True)
+trainer = BackpropTrainer(net,dataset = trainSet,learningrate = Learning,momentum = Momentum, verbose = True)
 trainer.trainOnDataset(trainSet,Ciclos)
-
-
 
 out = net.activateOnDataset(testSet)
 #print(out)
 out = out.argmax(axis=1) 
-print out
+#print out
 
 acerto = total = i = 0
 for data in testSet:
@@ -91,10 +87,10 @@ for data in testSet:
 	i += 1
 acuracia = float(float(acerto)/float(total))
 
-print repr(PorcDivTest)+" "+repr(Ciclos)+" "+repr(Momentum)+" "+repr(camada1)+" "+repr(camada2)+" "+repr(acuracia)
+print repr(PorcDivTest)+", "+repr(Ciclos)+", "+repr(Learning)+", "+repr(Momentum)+", "+repr(camada1)+", "+repr(camada2)+", "+repr(acuracia)
 
 outFile = open("outputFile.txt","a")
-outFile.write(repr(PorcDivTest)+" "+repr(Ciclos)+" "+repr(Momentum)+" "+repr(camada1)+" "+repr(camada2)+" "+repr(acuracia)+"\n")
+outFile.write(repr(PorcDivTest)+", "+repr(Ciclos)+", "+repr(Learning)+", "+repr(Momentum)+", "+repr(camada1)+", "+repr(camada2)+", "+repr(acuracia)+"\n")
 outFile.close()
 
 #trainer.trainOnDataset(trainSet,100)
